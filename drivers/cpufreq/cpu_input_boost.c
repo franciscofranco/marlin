44d9d664571baae048a6d60824b01b645b55c34a
Sultan Alsawaf <sultan@kerneltoast.com>
2019-03-04 10:52:26 -0800

    cpu_input_boost: Block external boosts when the screen is turned off
    It doesn't make sense to allow CPU boosts when the screen is off.
    
    Signed-off-by: Sultan Alsawaf <sultan@kerneltoast.com>
    Signed-off-by: Francisco Franco <franciscofranco.1990@gmail.com>

--- drivers/cpufreq/cpu_input_boost.c
+++ drivers/cpufreq/cpu_input_boost.c
@@ -105,6 +105,9 @@ void cpu_input_boost_kick(void)
 	if (!b)
 		return;
 
+	if (!(get_boost_state(b) & SCREEN_AWAKE))
+		return;
+
 	queue_work(b->wq, &b->input_boost);
 }
 
@@ -132,6 +135,9 @@ void cpu_input_boost_kick_max(unsigned int duration_ms)
 	struct boost_drv *b = boost_drv_g;
 
 	if (!b)
+		return;
+
+	if (!(get_boost_state(b) & SCREEN_AWAKE))
 		return;
 
 	__cpu_input_boost_kick_max(b, duration_ms);
